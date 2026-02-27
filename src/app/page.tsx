1cc10bdb932599c755d661a312df6e0d81e8e869
IgoorToorres <igorevangelista121@gmail.com>
2026-02-27 11:45:53 -0300

    feat:  estrutura inicial tela incial board

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,30 @@
+import { ArchiveIcon } from "lucide-react"
+
 export default function Home() {
   return (
-    <main>
-      <div>Hello world!</div>
-    </main>
-  );
+    <div className="max-w-405 w-full mx-auto p-10 flex flex-col gap-8 h-dvh">
+      <div />
+
+      <main className="grid grid-cols-4 gap-5 flex-1 items-stretch">
+        <div className="bg-navy-800 rounded-xl border-[0.5px] border-navy-500 pt-3 flex flex-col gap-1">
+          {/* header */}
+          <div className="flex items-center justify-between px-3">
+            <span className="bg-navy-700 rounded-lg px-3 py-1.5 flex items-center gap-2 text-xs">
+              <ArchiveIcon className="size-3" />
+              BackLog
+            </span>
+
+            <span className="text-navy-200 text-xs">12</span>
+          </div>
+
+          {/* content */}
+          <div className="flex flex-col gap-2.5 overflow-y-scroll p-3">
+            <div>card1</div>
+            <div>card2</div>
+            <div>card3</div>
+          </div>
+        </div>
+      </main>
+    </div>
+  )
 }
