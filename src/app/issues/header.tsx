3698f7ab4f57c3f4b0c0b3d081ecc117f44fa8b0
IgoorToorres <igorevangelista121@gmail.com>
2026-03-03 15:37:46 -0300

    feat: estrutura pagin issue

--- src/app/issues/header.tsx
+++ src/app/issues/header.tsx
@@ -1,5 +1,4 @@
 import UserButton from "@/components/user-button"
-import SearchInput from "./search-input"
 
 export default function Header() {
   return (
@@ -11,7 +10,6 @@ export default function Header() {
         </p>
       </div>
       <div className="flex items-center gap-4">
-        <SearchInput />
         <UserButton />
       </div>
     </div>
